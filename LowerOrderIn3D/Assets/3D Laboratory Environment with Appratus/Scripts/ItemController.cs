d24f1dec30d56001b412496b3adcc59c8fe9e972
migsngsn <migsngsn@gmail.com>
2025-03-01 23:19:12 +0800

    cleanout duplicate code

--- LowerOrderIn3D/Assets/3D Laboratory Environment with Appratus/Scripts/ItemController.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using static UnityEditor.Progress;
-
-public class ItemController : MonoBehaviour
-{
-    public Item Item;
-}
